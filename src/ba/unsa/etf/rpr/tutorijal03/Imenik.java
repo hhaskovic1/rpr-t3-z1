e2b023c971bc6d696dc15e43a159f7f196bf820d
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2018-10-31 22:22:43 +0100

    rad da Imenik klasi

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,6 +1,38 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.*;
+import java.util.HashMap;
+
 public class Imenik {
+
+    private HashMap<String, TelefonskiBroj> listaBrojeva = new HashMap<String, TelefonskiBroj>();
+
+    public void dodaj(String ime, TelefonskiBroj broj) {
+        listaBrojeva.put(ime, broj);
+    }
+
+    public String dajBroj(String ime) {
+        TelefonskiBroj broj = listaBrojeva.get(ime);
+        return broj.ispisi();
+
+    }
+
+    public String dajIme(TelefonskiBroj broj) {
+
+    }
+
+    String naSlovo(char s) {
+
+    }
+
+    Set<String> izGrada(FiksniBroj.Grad g) {
+
+    }
+
+    Set<TelefonskiBroj> izGradaBrojevi(FiksniBroj.Grad g) {
+        return null;
+    }
+
 
 
 
