f46039c130cac097b87c89cd2aacdc9fef7a30da
student <hhaskovic1@etf.unsa.ba>
2018-11-01 09:14:38 +0100

    rad na klasi MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -5,9 +5,19 @@ public class MobilniBroj extends TelefonskiBroj{
     private int mobilnaMreza;
     private String broj;
 
+    MobilniBroj() {}
+
     MobilniBroj(int mobilnaMreza, String broj) {
         this.mobilnaMreza=mobilnaMreza;
         this.broj=broj;
+    }
+
+    boolean compare(MobilniBroj m1) {
+        MobilniBroj m2 = new MobilniBroj();
+
+        if(m1.mobilnaMreza==m2.mobilnaMreza)return true;
+        else return false;
+
     }
 
 
--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -2,6 +2,16 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public abstract class TelefonskiBroj {
 
+
+    TelefonskiBroj() {}
+
+    TelefonskiBroj(int Comparable<TelefonskiBroj>)
+    {
+        int compareTo(TelefonskiBroj b) {
+            return(this.ispisi()compareTo(b.ispisi()));
+        }
+    }
+
     public abstract String ispisi();
     public abstract int hashCode();
 }
